e6f6920c0a28d81a500514e8ddc8787861204c98
Austin Marner <nivasmuthu452@gmail.com>
2020-06-01 10:26:11 +0530

    created struct for player to reduce functions for each player

--- main.go
+++ main.go
@@ -31,29 +31,23 @@ func needToContinue() (string, bool) {
 	case positions[0][2] == positions[1][1] && positions[0][2] == positions[2][0] && positions[0][2] != " ":
 		return positions[0][2], false
 	default:
-		return "", true
+		return " ", true
 	}
 }
 
-func player1Function() {
-	var x, y int16
-	fmt.Println("You are  X: ")
-	fmt.Print("Enter the x position ")
-	fmt.Scanf("%v", &x)
-	fmt.Print("Enter the y position: ")
-	fmt.Scanf("%v", &y)
-	fmt.Println(x, y)
-	positions[x][y] = "X"
+type player struct {
+	name   string
+	avatar string
 }
 
-func player2Function() {
+func (p player) playerFunction() {
 	var x, y int16
-	fmt.Println("Yoe are O: ")
+	fmt.Printf("You are %s\n", string(p.avatar))
 	fmt.Print("Enter the x position ")
 	fmt.Scanf("%v", &x)
 	fmt.Print("Enter the y position: ")
 	fmt.Scanf("%v", &y)
-	positions[x][y] = "O"
+	positions[x][y] = p.avatar
 }
 
 func printPosition() {
@@ -73,8 +67,10 @@ func printPosition() {
 }
 
 func main() {
+
 	initialize()
-	var player1, player2 string
+	var player1, player2 player
+	var temp string
 	fmt.Println("Welcome to TicTacGo")
 	fmt.Println(`Board Position 
 
@@ -90,23 +86,25 @@ func main() {
 
 	`)
 	fmt.Print("Enter the name of player 1: ")
-	fmt.Scanln(&player1)
+	fmt.Scanln(&temp)
+	player1 = player{temp, "X"}
 	fmt.Print("Enter the name of player 2: ")
-	fmt.Scanln(&player2)
+	fmt.Scanln(&temp)
+	player2 = player{temp, "O"}
 	fmt.Println("Game Starts")
 	for {
-		player1Function()
+		player1.playerFunction()
 		printPosition()
 		if k, v := needToContinue(); v == false {
 			switch k {
 			case "X":
-				fmt.Printf("Congrats %s on winning\n", player1)
+				fmt.Printf("Congrats %s on winning\n", player1.name)
 			case "O":
-				fmt.Printf("Congrats %s on winning\n", player2)
+				fmt.Printf("Congrats %s on winning\n", player2.name)
 			}
 			break
 		}
-		player2Function()
+		player2.playerFunction()
 		printPosition()
 		if k, v := needToContinue(); v == false {
 			switch k {
@@ -118,4 +116,5 @@ func main() {
 			break
 		}
 	}
+
 }
